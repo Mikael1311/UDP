e965d646a65d8b650452c7d9d1228a7e40f1a92d
cpatrone503 <71048515+cpatrone503@users.noreply.github.com>
2020-09-30 18:17:39 -0400

    Update UDP_client.cpp

--- UDP_client.cpp
+++ UDP_client.cpp
@@ -1,34 +1,29 @@
 /*  Christopher Patrone
+    Mika Hamiti
+    Saja Freeman
     Network Design
-    Phase 1
+    Phase 2
     This program is written in C++, and the goal is to create a UDP server and client that
     can send messages and back and forth.
-
     This file is for the UDP client code.
-
     REFERENCES:
-
     [1]  Kelly, Sloan. Sloan Kelly. "Starter UDP Server and Client in C++".
          Youtube. 2017. URL: https://www.youtube.com/watch?v=uIanSvWou1M
-
     [2]  mattew99g. Guided Hacking. "C++ Winsock Networking Tutorial - Introduction".
          Youtube. 2019. URL: https://www.youtube.com/watch?v=GiOgWfmWzPY
-
     [3]  Tougher, Rob. Linux Gazette. "Linux Socket Programming in C++".
          tldp.org. 2002. URL: https://tldp.org/LDP/LG/issue74/tougher.html#4
-
     [4]  Hall, Brian. Beej's Guide to Network Programming. "Using Internet Sockets".
          beej.us. 2019. URL: https://beej.us/guide/bgnet/html/#windows
-         
+
     [5]  Cipher Deprogres. "C++ Winsock Transfer Data with Socket". Youtube. 2018.
          URL: https://www.youtube.com/watch?v=NHrk33uCzL8
-
 */
 
 #include <iostream>
 #include <ws2tcpip.h>
 #include <string>
-#include <sys/sendfile.h>
+//#include <sys/sendfile.h>
 #include <fstream> ///// For opening files
 
 #pragma comment (lib, "ws2_32.lib")  // Add winsock library
@@ -56,7 +51,7 @@ void main(int argc, char* argv[])  // Command line input
 
     // Create a socket
     SOCKET sock2 = socket(AF_INET, SOCK_DGRAM, 0);
-    
+
     // Write out to socket
     string s(argv[1]);
     int sendOK = sendto(sock2, s.c_str(), s.size() + 1, 0, (sockaddr*)&server, sizeof(server));  // Send command line argument to server
@@ -69,7 +64,7 @@ void main(int argc, char* argv[])  // Command line input
     cout << "SERVER SUCCESSFULLY RECEIVED MESSAGE: " << buff << endl;
     ZeroMemory(buff, 1024);  // Zero the buffer
 
-    
+
     ///// Ask for user input message
     char command;
     string s2;
@@ -77,7 +72,7 @@ void main(int argc, char* argv[])  // Command line input
     ofstream file;  ///// File to open
     char fBuff[1024];  ///// Buffer array for file
     int fileDownloaded = 0;  ///// Keep track of file being downloaded
-    
+
     cout << "Press m to enter message, f to enter a file name, x to exit" << endl;
     cin >> command;
     cin.ignore(256, '\n');  // Will the user send a message, or exit the client program
@@ -100,8 +95,8 @@ void main(int argc, char* argv[])  // Command line input
             ZeroMemory(buff2, 1024);  // Zero the buffer
 
         }
-        
-        
+
+
         ///// SENDING A FILE
         ///// User inputs "f" to send a file
         else if (command == 'f') {
@@ -131,13 +126,13 @@ void main(int argc, char* argv[])  // Command line input
             } while (fileDownloaded < FileName.size());
             file.close();
         }
-        
-        
+
+
         cout << "Press m to enter message, f to enter a file name, x to exit" << endl;  // Ask if user wants to send another message
         cin >> command;
         cin.ignore(256, '\n');
     }
-    
+
 
     // Close the socket
     closesocket(sock2);
